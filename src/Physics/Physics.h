ea17161f9bf4ac96253991685a5344976f9a39b6
Rostyslav Onikiienko <ronikiienko@gmail.com>
2023-10-10 09:49:21 -0700

    solve contact func separate

--- src/Physics/Physics.h
+++ src/Physics/Physics.h
@@ -31,6 +31,20 @@ private:
         }
     }
 
+    void solveContact(VerletObject& obj1, VerletObject& obj2) {
+        const Vector2 vectorBetween = obj1.posCurr - obj2.posCurr;
+        const float dist2 = vectorBetween.magnitude2();
+        const float min_dist = obj1.radius + obj2.radius;
+        // Check overlapping
+        if (dist2 < min_dist * min_dist) {
+            const float dist = sqrt(dist2);
+            const Vector2 normal = vectorBetween / dist;
+            const float delta = 0.5f * collisionsDamping * (dist - min_dist);
+            // Update positions
+            obj1.posCurr -= normal * delta;
+            obj2.posCurr += normal * delta;
+        }
+    }
 
     void solveCollisionsNoGrid() {
         std::vector<VerletObject> &objects = world.getObjects();
@@ -39,18 +53,7 @@ private:
             VerletObject& obj1 = objects[i];
             for (int j = i + 1; j < objectsCount; j++) {
                 VerletObject& obj2 = objects[j];
-                const Vector2 vectorBetween = obj1.posCurr - obj2.posCurr;
-                const float dist2 = vectorBetween.magnitude2();
-                const float min_dist = obj1.radius + obj2.radius;
-                // Check overlapping
-                if (dist2 < min_dist * min_dist) {
-                    const float dist = sqrt(dist2);
-                    const Vector2 normal = vectorBetween / dist;
-                    const float delta = 0.5f * collisionsDamping * (dist - min_dist);
-                    // Update positions
-                    obj1.posCurr -= normal * delta;
-                    obj2.posCurr += normal * delta;
-                }
+                solveContact(obj1, obj2);
             }
         }
     }
@@ -78,7 +81,7 @@ public:
         for (int i = 0; i < physicsSubSteps; i++) {
             applyGravity();
             applyConstraints();
-//            solveCollisionsNoGrid();
+            solveCollisionsNoGrid();
             updatePositions(subStepDt);
         }
     }
