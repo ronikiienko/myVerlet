0b6bc45fe2b687b7c09e777f4003ca04e1c2974c
Rostyslav Onikiienko <ronikiienko@gmail.com>
2023-10-10 11:17:10 -0700

    save

--- src/consts.h
+++ src/consts.h
@@ -3,13 +3,13 @@
 #include "modules/Vector.h"
 #include "modules/Rectangle.h"
 
-constexpr Vector2 gravity = Vector2::fromCartesian(0, 2);
+constexpr Vector2 gravity = Vector2::fromCartesian(0, 1);
 
-const RectangleI worldBounds = RectangleI::fromSize(0,0,1000,1000);
-const RectangleI windowBounds = RectangleI::fromSize(0,0,1000,1000);
+const RectangleI worldBounds = RectangleI::fromSize(0,0,1400,1400);
+const RectangleI windowBounds = RectangleI::fromSize(0,0,1400,1400);
 
 const float physicsInterval = 0.016f;
-const int physicsSubSteps = 8;
+const int physicsSubSteps = 4;
 
 const float wallsDamping = 0.75f;
 const float collisionsDamping = 0.8f;
