705f8920079ae574a2d3d21de24f4b44ba0baa1d
Rostyslav Onikiienko <ronikiienko@gmail.com>
2023-10-10 16:12:10 -0700

    bas

--- src/consts.h
+++ src/consts.h
@@ -9,7 +9,7 @@ const RectangleI worldBounds = RectangleI::fromSize(0,0,1400,1400);
 const RectangleI windowBounds = RectangleI::fromSize(0,0,1400,1400);
 
 const float physicsInterval = 0.016f;
-const int physicsSubSteps = 4;
+const int physicsSubSteps = 8;
 
-const float wallsDamping = 0.75f;
+const float wallsDamping = 0.8f;
 const float collisionsDamping = 0.8f;
