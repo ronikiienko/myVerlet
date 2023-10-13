a48eaa9ee320574de65477e412dc343c61f24b3f
Rostyslav Onikiienko <ronikiienko@gmail.com>
2023-10-12 17:10:36 -0700

    normal number of objects

--- src/consts.h
+++ src/consts.h
@@ -3,16 +3,16 @@
 #include "modules/Vector.h"
 #include "modules/Rectangle.h"
 
-const int maxObjectNum = 90000;
+const int maxObjectNum = 100000;
 const int maxSticksNum = 100;
 
 constexpr Vector2 gravity = Vector2::fromCartesian(0, 100);
 
-const RectangleI worldBounds = RectangleI::fromSize(0,0,3500,1500);
-const RectangleI windowBounds = RectangleI::fromSize(0,0,3500,1500);
+const RectangleI worldBounds = RectangleI::fromSize(0,0,2000,1000);
+const RectangleI windowBounds = RectangleI::fromSize(0,0,2000,1000);
 
-const int minRadius = 4;
-const int maxRadius = 4;
+const int minRadius = 2;
+const int maxRadius = 2;
 
 const int collisionGridWidth = worldBounds.getWidth() / (maxRadius * 2);
 const int collisionGridHeight = worldBounds.getHeight() / (maxRadius * 2);
