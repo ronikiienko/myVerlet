f89bcd1f91a136a823db9bd0575f4b6382aa672e
Rostyslav Onikiienko <ronikiienko@gmail.com>
2023-10-13 10:00:24 -0700

    wip: make rectangle methods constexpr

--- src/modules/Rectangle.h
+++ src/modules/Rectangle.h
@@ -5,43 +5,43 @@ class Rectangle {
 private:
     T x1, x2, y1, y2;
 
-    Rectangle(T x1, T y1, T x2, T y2) : x1(x1), x2(x2), y1(y1), y2(y2) {};
+    constexpr Rectangle(T x1, T y1, T x2, T y2) : x1(x1), x2(x2), y1(y1), y2(y2) {};
 public:
 
-    static Rectangle fromCoords(T x1, T y1, T x2, T y2) {
+    constexpr static Rectangle fromCoords(T x1, T y1, T x2, T y2) {
         return {x1, y1, x2, y2};
     }
 
-    static Rectangle fromSize(T x1, T y1, T width, T height) {
+    constexpr static Rectangle fromSize(T x1, T y1, T width, T height) {
         return {x1, y1, x1 + width, y1 + height};
     }
 
     template<typename U>
-    static Rectangle fromOther(Rectangle<U>& other) {
+    constexpr static Rectangle fromOther(Rectangle<U>& other) {
         return {static_cast<T>(other.getX1()), static_cast<T>(other.getY1()), static_cast<T>(other.getX2()), static_cast<T>(other.getY2())};
     }
 
-    [[nodiscard]] T getX1() const {
+    [[nodiscard]] constexpr T getX1() const {
         return x1;
     }
 
-    [[nodiscard]] T getX2() const {
+    [[nodiscard]] constexpr T getX2() const {
         return x2;
     }
 
-    [[nodiscard]] T getY1() const {
+    [[nodiscard]] constexpr T getY1() const {
         return y1;
     }
 
-    [[nodiscard]] T getY2() const {
+    [[nodiscard]] constexpr T getY2() const {
         return y2;
     }
 
-    [[nodiscard]] T getWidth() const {
+    [[nodiscard]] constexpr T getWidth() const {
         return x2 - x1;
     }
 
-    [[nodiscard]] T getHeight() const {
+    [[nodiscard]] constexpr T getHeight() const {
         return y2 - y1;
     }
 };
