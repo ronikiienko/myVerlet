a0f31493e478f128d88db2044d014c0756be768a
Rostyslav Onikiienko <ronikiienko@gmail.com>
2023-10-13 10:01:52 -0700

    make some constants constexpr

--- src/consts.h
+++ src/consts.h
@@ -8,14 +8,14 @@ const int maxSticksNum = 100000;
 
 constexpr Vector2 gravity = Vector2::fromCartesian(0, 50);
 
-const RectangleI worldBounds = RectangleI::fromSize(0,0,2500,1200);
-const RectangleI windowBounds = RectangleI::fromSize(0,0,2500,1200);
+constexpr RectangleI worldBounds = RectangleI::fromSize(0,0,2500,1200);
+constexpr RectangleI windowBounds = RectangleI::fromSize(0,0,2500,1200);
 
-const int minRadius = 2;
-const int maxRadius = 2;
+constexpr int minRadius = 2;
+constexpr int maxRadius = 2;
 
-const int collisionGridWidth = worldBounds.getWidth() / (maxRadius * 2);
-const int collisionGridHeight = worldBounds.getHeight() / (maxRadius * 2);
+constexpr int collisionGridWidth = worldBounds.getWidth() / (maxRadius * 2);
+constexpr int collisionGridHeight = worldBounds.getHeight() / (maxRadius * 2);
 
 const float physicsInterval = 0.016f;
 const int physicsSubSteps = 8;
