ea5761849ab5ac5c74428a0fcdcf2be31b20b2ec
Rostyslav Onikiienko <ronikiienko@gmail.com>
2023-10-14 19:59:11 -0700

    collision solver cleanup function

--- src/consts.h
+++ src/consts.h
@@ -16,7 +16,7 @@ constexpr int maxRadius = 2;
 
 // collision grid when finding collisions is split on many threads (by columns). To avoid race conditions i use two pass method. This means that grid in fact will be split on numberOfThreads * 2 tasks. So, if we have 10threads
 // and grid of, say, size 39 will mean that each thread gets 1 task, and 19 remain for "cleanup" function, which is inefficient, because cleanup function is not really good
-// best if grid is divided by threads * 2 without remainder
+// Conclusion: best if grid is divided by threads * 2 without remainder
 constexpr int collisionGridWidth = worldBounds.getWidth() / (maxRadius * 2);
 constexpr int collisionGridHeight = worldBounds.getHeight() / (maxRadius * 2);
 
