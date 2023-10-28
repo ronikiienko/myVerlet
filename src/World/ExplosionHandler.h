3d0f0bd609b26cea079152c26f5eec3c7757326b
Rostyslav Onikiienko <ronikiienko@gmail.com>
2023-10-27 19:22:35 -0700

    ExplosionHandler.h class

---
+++ src/World/ExplosionHandler.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "World.h"
+
+class ExplosionHandler {
+private:
+    World& world;
+public:
+    explicit ExplosionHandler(World& world) : world(world) {}
+
+    void launch(Vector2 position, float strength, float radius) {
+        std::vector<VerletObject>& objects = world.getObjects();
+        for (auto& object : objects) {
+            Vector2 direction = object.posCurr - position;
+            float distance = direction.magnitude();
+            if (distance <= radius) {
+                const float individualStrength = strength * distance / radius;
+                Vector2 addedVelocity = direction / distance * individualStrength;
+                object.addVelocity(addedVelocity);
+            }
+        }
+    }
+};
