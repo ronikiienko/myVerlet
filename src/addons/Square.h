8ffe879f4b70a237f757eb3898bb83467500574f
Rostyslav Onikiienko <ronikiienko@gmail.com>
2023-10-29 23:15:22 -0700

    square class

---
+++ src/addons/Square.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include "../modules/Vector.h"
+#include "../World/World.h"
+#include "../consts.h"
+
+class Square {
+public:
+    Square(World& world, Vector2 startPos, float size) {
+        int ind1 = world.addObject(startPos, maxRadius);
+        int ind2 = world.addObject(startPos + Vector2::fromCartesian(size,0), maxRadius);
+        int ind3 = world.addObject(startPos + Vector2::fromCartesian(size,size), maxRadius);
+        int ind4 = world.addObject(startPos + Vector2::fromCartesian(0,size), maxRadius);
+
+        world.addStick(ind1, ind2);
+        world.addStick(ind2, ind3);
+        world.addStick(ind3, ind4);
+        world.addStick(ind4, ind1);
+        world.addStick(ind1, ind3);
+        world.addStick(ind4, ind2);
+    }
+};
