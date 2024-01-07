31fea9657ed1299ce7f151041d957011392d46d0
Rostyslav Onikiienko <ronikiienko@gmail.com>
2024-01-06 20:44:09 -0800

    remove stuff from level

--- src/Level.h
+++ src/Level.h
@@ -1,15 +1,12 @@
 #pragma once
 
 #include "Engine/BaseLevel.h"
-#include "addons/ExplosionHandler.h"
-#include "addons/Shooter.h"
 #include "Engine/utils/Rand.h"
 #include "addons/RandomSpawner.h"
-#include "Objects/Player.h"
 
 class Level : public BaseLevel {
-    ExplosionHandler explosionHandler{atomWorld};
-    Shooter shooter{Vector2::fromCartesian(200, 200), 2, 1, 4, atomWorld, 1, explosionHandler};
+//    ExplosionHandler explosionHandler{atomWorld};
+//    Shooter shooter{Vector2::fromCartesian(200, 200), 2, 1, 4, atomWorld, 1, explosionHandler};
     RNGf gen{consts::seed};
     RandomSpawner randomSpawner{atomWorld, gen};
 public:
@@ -18,31 +15,31 @@ public:
 
     void onInit() override {
         randomSpawner.spawn(80000);
-        int id = atomWorld.addObject(Player{Vector2::fromCartesian(100, 100), inputHandler, camera, shooter});
-        inputHandler.addEventListener(sf::Event::MouseWheelScrolled, [&](sf::Event &event) {
-            if (event.mouseWheelScroll.delta > 0) {
-                camera.zoom(1.5);
-            } else {
-                camera.zoom(0.75);
-            }
-        });
-        inputHandler.addEventListener(sf::Event::KeyPressed, [&](sf::Event &event) {
-            if (event.key.code == sf::Keyboard::BackSpace) {
-                atomWorld.clear();
-            }
-
-            if (event.key.code == sf::Keyboard::M) {
-                randomSpawner.spawn(5000);
-            }
-        });
-        inputHandler.addEventListener(sf::Event::MouseButtonPressed, [&](sf::Event &event) {
-            if (event.mouseButton.button == sf::Mouse::Right) {
-                explosionHandler.launch(
-                        camera.screenPosToWorldPos(Vector2::fromCartesian(static_cast<float>(event.mouseButton.x),
-                                                                          static_cast<float>(event.mouseButton.y))),
-                        4,
-                        150);
-            }
-        });
+//        int id = atomWorld.addObject(Player{Vector2::fromCartesian(100, 100), inputHandler, camera, shooter});
+//        inputHandler.addEventListener(sf::Event::MouseWheelScrolled, [&](sf::Event &event) {
+//            if (event.mouseWheelScroll.delta > 0) {
+//                camera.zoom(1.5);
+//            } else {
+//                camera.zoom(0.75);
+//            }
+//        });
+//        inputHandler.addEventListener(sf::Event::KeyPressed, [&](sf::Event &event) {
+//            if (event.key.code == sf::Keyboard::BackSpace) {
+//                atomWorld.clear();
+//            }
+//
+//            if (event.key.code == sf::Keyboard::M) {
+//                randomSpawner.spawn(5000);
+//            }
+//        });
+//        inputHandler.addEventListener(sf::Event::MouseButtonPressed, [&](sf::Event &event) {
+//            if (event.mouseButton.button == sf::Mouse::Right) {
+//                explosionHandler.launch(
+//                        camera.screenPosToWorldPos(Vector2::fromCartesian(static_cast<float>(event.mouseButton.x),
+//                                                                          static_cast<float>(event.mouseButton.y))),
+//                        4,
+//                        150);
+//            }
+//        });
     }
 };
