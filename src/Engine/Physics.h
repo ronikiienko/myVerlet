6617f3ced8b259fa7b07c8531a8e90ea70458d5f
Rostyslav Onikiienko <ronikiienko@gmail.com>
2024-01-06 21:45:08 -0800

    calculate current substep

--- src/Engine/Physics.h
+++ src/Engine/Physics.h
@@ -11,6 +11,7 @@ private:
     IdGrid grid;
     ThreadPool &threadPool;
     PerformanceMonitor &performanceMonitor;
+    int currentSubStep = 0;
 
     void updatePositionsConstraint(float dt) {
         const Rectangle bounds = atomWorld.getBoundsF();
@@ -20,7 +21,7 @@ private:
             const float maxX = bounds.getX2() - consts::objectsRadius;
             const float minY = bounds.getY1() + consts::objectsRadius;
             const float maxY = bounds.getY2() - consts::objectsRadius;
-            atomWorld.forEachBasicDetails([&bounds, dt, minX, maxX, minY, maxY](BasicDetails& object, int i) {
+            atomWorld.forEachBasicDetails([&bounds, dt, minX, maxX, minY, maxY](BasicDetails &object, int i) {
                 // TODO when all grid filled with objects, you can see that some start falling faster and some slower (on lower gravity levels like 10) - this is because of floats precision
                 if (!object.isPinned) {
                     object.accelerate(consts::gravity);
@@ -96,13 +97,14 @@ private:
 //        });
 //    }
 
-    static void solveContact(BasicDetails &obj1, BasicDetails &obj2) {
+    void solveContact(BasicDetails &obj1, BasicDetails &obj2) {
         const Vector2 vectorBetween = obj1.posCurr - obj2.posCurr;
         const float dist2 = vectorBetween.magnitude2();
         // Check overlapping
         if (dist2 < consts::twoObjectsRadiusSquared) {
             obj1.parent->onCollision();
             obj2.parent->onCollision();
+
             const float dist = std::sqrt(dist2);
             if (dist == 0) return;
             const Vector2 normal = vectorBetween / dist;
@@ -172,7 +174,7 @@ private:
         const int sliceSize = grid.width / sliceCount;
         //  to avoid data races - process in two passes. So that no threads are assigned to neighbouring columns
         for (int i = 0; i < threadCount; i++) {
-            threadPool.addTask([i, sliceSize, this](){
+            threadPool.addTask([i, sliceSize, this]() {
                 int startCol = (i * 2) * sliceSize;
                 int endCol = startCol + sliceSize;
                 solveCollisionsSubgrid(startCol, endCol, 0, grid.height);
@@ -190,7 +192,7 @@ private:
         threadPool.waitForCompletion();
 
         if (sliceSize * sliceCount < grid.width) {
-            threadPool.addTask([sliceSize, sliceCount, this](){
+            threadPool.addTask([sliceSize, sliceCount, this]() {
                 int startCol = sliceSize * sliceCount;
                 int endCol = grid.width;
                 solveCollisionsSubgrid(startCol, endCol, 0, grid.height);
@@ -198,9 +200,6 @@ private:
         }
         threadPool.waitForCompletion();
     }
-
-
-
 
 
 //    void rebuildGrid() {
@@ -213,12 +212,12 @@ private:
 //    }
     void rebuildGrid() {
         performanceMonitor.start("grid clear");
-        threadPool.dispatch(grid.length, [this](int start, int end){
+        threadPool.dispatch(grid.length, [this](int start, int end) {
             grid.clear(start, end);
         });
         performanceMonitor.end("grid clear");
         performanceMonitor.start("grid build");
-        atomWorld.forEachBasicDetails([this](BasicDetails& object, int i) {
+        atomWorld.forEachBasicDetails([this](BasicDetails &object, int i) {
             grid.insert(i, object.posCurr.x, object.posCurr.y);
         });
         performanceMonitor.end("grid build");
@@ -226,13 +225,16 @@ private:
     }
 
 public:
-    explicit Physics(AtomWorld &atomWorld, ThreadPool &threadPool, PerformanceMonitor& performanceMonitor)
-            : atomWorld(atomWorld), grid(consts::collisionGridWidth, consts::collisionGridHeight, atomWorld.getBoundsI()), threadPool(threadPool), performanceMonitor(performanceMonitor) {}
+    explicit Physics(AtomWorld &atomWorld, ThreadPool &threadPool, PerformanceMonitor &performanceMonitor)
+            : atomWorld(atomWorld),
+              grid(consts::collisionGridWidth, consts::collisionGridHeight, atomWorld.getBoundsI()),
+              threadPool(threadPool), performanceMonitor(performanceMonitor) {}
 
     void update() {
         const float subStepDt = consts::physicsInterval / consts::physicsSubSteps;
 
         for (int i = 0; i < consts::physicsSubSteps; i++) {
+            currentSubStep = i;
 
             performanceMonitor.start("gravityConstraintsUpdate");
             updatePositionsConstraint(subStepDt);
--- src/Level.h
+++ src/Level.h
@@ -15,7 +15,7 @@ public:
 
     void onInit() override {
         atomWorld.addObject(BaseObject(), Vector2::fromCartesian(100, 100));
-        randomSpawner.spawn(100000);
+        randomSpawner.spawn(150000);
 //        int id = atomWorld.addObject(Player{Vector2::fromCartesian(100, 100), inputHandler, camera, shooter});
 //        inputHandler.addEventListener(sf::Event::MouseWheelScrolled, [&](sf::Event &event) {
 //            if (event.mouseWheelScroll.delta > 0) {
