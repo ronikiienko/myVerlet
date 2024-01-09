eaa2d490c83acbe6cb303570eda22538def00a99
Rostyslav Onikiienko <ronikiienko@gmail.com>
2024-01-08 16:58:22 -0800

    implement EventHandler.h

---
+++ src/Engine/EventHandler.h
@@ -0,0 +1,38 @@
+#pragma once
+
+#include <unordered_map>
+#include <functional>
+#include <typeindex>
+
+struct BaseEvent {
+    BaseEvent() = default;
+    virtual ~BaseEvent() = default;
+    virtual void print() const = 0;
+};
+
+class EventManager {
+    std::unordered_map<std::type_index, std::unordered_map<int, std::function<void(const BaseEvent &)>>> callbacks;
+    int keyCounter = 0;
+public:
+    template<typename EventType>
+    int addEventListener(const std::function<void(const EventType &)> &callback) {
+        // here passing callback as copy is necessary. otherwise if parameter is a reference and it is destroyed, lambda will have dangling reference
+        auto wrapper = [callback](const BaseEvent &baseEvent) {
+            callback(static_cast<const EventType &>(baseEvent));
+        };
+        callbacks[typeid(EventType)][keyCounter] = wrapper;
+        return keyCounter++;
+    }
+
+    template<typename EventType>
+    void removeEventListener(int key) {
+        callbacks[typeid(EventType)].erase(key);
+    }
+
+    void emit(const BaseEvent &event) {
+        auto &eventTypeCallbacks = callbacks[typeid(event)];
+        for (auto &pair: eventTypeCallbacks) {
+            pair.second(event);
+        }
+    }
+};
