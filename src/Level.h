8103e89cc84215685d68b5f95dbbdbb1beda1da2
Rostyslav Onikiienko <ronikiienko@gmail.com>
2024-01-17 00:46:32 -0800

    spawn many objects, again

--- src/Level.h
+++ src/Level.h
@@ -17,7 +17,7 @@ public:
     }
 
     void v_onInit() override {
-//        m_randomSpawner.spawn(150000);
+        m_randomSpawner.spawn(150000);
         m_scene.addObject(Player{m_inputHandler, m_scene}, Vector2F::cart(100, 100));
         std::cout << "Objects num: " << m_scene.getObjectsCount() << std::endl;
 
