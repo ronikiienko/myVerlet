c611bd86ac7fd63f7eb436b9c6cc8a38a505fd1e
Rostyslav Onikiienko <ronikiienko@gmail.com>
2024-03-03 15:31:14 -0800

    replace RandomSpawner by RandomPositionGenerator

---
+++ src/Engine/Modules/RandomPositionGenerator.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "../Scene.h"
+#include "../utils/Rand.h"
+
+class RandomPositionGenerator {
+private:
+    Scene &m_scene;
+    RNGf& m_gen;
+public:
+    RandomPositionGenerator(Scene &scene, RNGf& gen) : m_scene(scene), m_gen(gen) {}
+
+    Vector2F get() {
+        return Vector2F::cart(m_gen.getInRange(0, m_scene.getSizeF().m_x), m_gen.getInRange(0, m_scene.getSizeF().m_y));
+    }
+};
--- src/GameTest/Levels/Level1.h
+++ src/GameTest/Levels/Level1.h
@@ -2,23 +2,26 @@
 
 #include "../../Engine/BaseLevel.h"
 #include "../../Engine/utils/Rand.h"
-#include "../../addons/RandomSpawner.h"
 #include "../Entities/Player.h"
 #include "../consts.h"
 #include "../Entities/Enemy.h"
 #include "../../Engine/Modules/ExplosionHandler.h"
+#include "../../Engine/Modules/RandomPositionGenerator.h"
+#include "../Entities/EmptyObject.h"
 
 class Level1 : public BaseLevel {
     ExplosionHandler m_explosionHandler{m_scene};
     RNGf m_gen{consts::seed};
-    RandomSpawner m_randomSpawner{m_scene, m_gen};
+    RandomPositionGenerator m_randomPositionGenerator{m_scene, m_gen};
     SMHandle m_firstSound;
 public:
     explicit Level1(LevelContext levelContext) : BaseLevel(levelContext, 200000, Vector2I::cart(2000, 2000), 600) {
     }
 
     void v_onInit() override {
-        m_randomSpawner.spawn(150000);
+        for (int i = 0; i < 150000; i++) {
+            m_scene.addObject(EmptyObject{m_scene.getObjectContext()}, m_randomPositionGenerator.get());
+        }
         auto playerPtr = m_scene.addObject(Player{m_scene.getObjectContext(), m_inputBus, m_window, m_gen},
                                            Vector2F::cart(102, 102));
 //        for (int i = 0; i < 1000; i++) {
