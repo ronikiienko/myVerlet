817cb484f422deb457761297eb2963087380be7c
Rostyslav Onikiienko <ronikiienko@gmail.com>
2024-03-11 13:04:31 -0700

    fix: remove bad dependencies from PerformanceTest

--- src/Engine/Modules/PerformanceTest/PerformanceTest.h
+++ src/Engine/Modules/PerformanceTest/PerformanceTest.h
@@ -2,16 +2,16 @@
 
 #include "../../BaseLevel.h"
 #include "../../utils/Rand.h"
-#include "../../Entities/Player.h"
-#include "../../../GameTest/consts.h"
-#include "../../Entities/Enemy.h"
 #include "../ExplosionHandler.h"
 #include "EmptyObject.h"
+#include "../RandomPositionGenerator.h"
+#include "../CameraControls.h"
 
 class PerformanceTest : public BaseLevel {
     ExplosionHandler m_explosionHandler{m_scene};
-    RNGf m_gen{consts::seed};
+    RNGf m_gen{10};
     RandomPositionGenerator m_randomPositionGenerator{m_scene, m_gen};
+    CameraControls m_cameraControls{m_scene, m_inputBus};
 public:
     explicit PerformanceTest(LevelContext levelContext) : BaseLevel(levelContext, 200000, Vector2I::cart(2000, 2000), 600) {
     }
@@ -20,7 +20,6 @@ public:
         for (int i = 0; i < 150000; i++) {
             m_scene.addObject(EmptyObject{m_scene.getObjectContext()}, m_randomPositionGenerator.get());
         };
-        auto playerPtr = m_scene.addObject(Player{m_scene.getObjectContext(), m_inputBus, m_window, m_gen}, Vector2F::cart(100, 100));
     }
     void v_onTick() override {}
 };
